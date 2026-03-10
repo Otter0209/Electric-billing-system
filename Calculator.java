f25e2e81e686356a69b2a11792a9e33539a1de62
MaMinky <141635331+MaMinky@users.noreply.github.com>
2026-03-10 07:27:27 +0700

    Change BASE_PRICE to 2000.0 for calculation

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     // CHÚ Ý: Đây là dòng để tạo Merge Conflict khi thuyết trình!
     // Bạn Hoài sẽ đổi thành 2000.0 trên nhánh main
     // Bạn Tiến sẽ đổi thành 1800.0 trên nhánh tính năng (feature branch)
-    private static final double BASE_PRICE = 1500.0; 
+    private static final double BASE_PRICE = 2000.0; 
 
     public double calculateBill(double kwh) {
         System.out.println("Đang tính toán với đơn giá: " + BASE_PRICE + " VND/kWh");
