2cd3d609177dae27bbcfd90ce75de8057730ba5e
Otter0209 <thichanmuc0209@gmail.com>
2026-03-13 07:33:35 +0700

    doi gia tien

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,6 @@
 public class Calculator {
     
-    // CHÚ Ý: Đây là dòng để tạo Merge Conflict khi thuyết trình!
-    // Bạn Hoài sẽ đổi thành 2000.0 trên nhánh main
-    // Bạn Tiến sẽ đổi thành 1800.0 trên nhánh tính năng (feature branch)
+
     private static final double BASE_PRICE = 1800.0; 
 
     public double calculateBill(double kwh) {
--- Main.java
+++ Main.java
@@ -4,16 +4,16 @@ public class Main {
         System.out.println("  HỆ THỐNG QUẢN LÝ HÓA ĐƠN ĐIỆN     ");
         System.out.println("===============================================");
 
-        // 1. Quản lý khách hàng
+        
         CustomerManager manager = new CustomerManager();
         manager.addCustomer("Nguyễn Văn A");
         manager.addCustomer("Trần Thị B");
         
         System.out.println("-----------------------------------------------");
 
-        // 2. Tính tiền điện
+    
         Calculator calc = new Calculator();
-        double usage = 150.5; // Giả sử dùng 150.5 ký điện
+        double usage = 150.5;
         double total = calc.calculateBill(usage);
 
         System.out.println("Tổng số điện tiêu thụ: " + usage + " kWh");
